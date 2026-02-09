79a104c49155522158e589fb9313723b00ad2ed1
DESKTOP-62UBN2T\beyza aktas <azyebaktas@gmail.com>
2026-02-09 17:50:26 +0300

    fix: navbar links and page routing

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const express =require('express');
-const app=express();
-app.get('/',(req,res)=>{
-    res.send('index sayfasi')
+const express = require('express');
+const pageRoute = require('./routes/pageRoute');
 
-})
-const port=3000;
-app.listen(port,()=>{
-    console.log("app started on port ${port}")
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// GLOBAL VARIABLE
+app.use((req, res, next) => {
+  res.locals.page_name = '';
+  next();
+});
+
+app.use('/', pageRoute);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
 });
---
+++ routes/pageRoute.js
@@ -0,0 +1,6 @@
+const express=require('express');
+const router = express.Router();
+const pageController=require('../controllers/pageController');
+router.route('/').get(pageController.getIndexPage);
+router.route('/about').get(pageController.getAboutPage);
+module.exports=router;
